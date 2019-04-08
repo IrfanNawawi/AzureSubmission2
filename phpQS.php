4bfa2c129cb9a001093aab2dfb40741975194a02
Irfan Nawawi <37669007+IrfanNawawi@users.noreply.github.com>
2019-04-08 16:46:15 +0700

    Update phpQS.php

--- phpQS.php
+++ phpQS.php
@@ -47,7 +47,7 @@ $connectionString = "DefaultEndpointsProtocol=https;AccountName=cripsstorage;Acc
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
-$fileToUpload = "HelloWorld.txt";
+$fileToUpload = "images/Sertifikat MADE - Dicoding.png";
 
 if (!isset($_GET["Cleanup"])) {
     // Create container options object.
